f46ce93d2e3ec4c59b7e4febb42d2260e462f334
Sasa Brankovic <sasa@hakeraj.com>
2010-02-20 19:01:03 +0100

    Cacheify specs

--- spec/cacheify_spec.rb
+++ spec/cacheify_spec.rb
@@ -1,7 +1,28 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+# Example class we'll use in specs
+class Foo 
+  def bar(something = nil)
+    "#{rand(99999999)}#{something}"
+  end
+end
+
 describe "Cacheify" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  before :all do
+    Foo.extend Cacheify
+    Foo.cache_method :bar, :hello => "Friend"
+    
+    @foo = Foo.new
+    @first_call = @foo.bar("hello")
+  end
+
+  context "calling method twice on a cachified object" do
+    before do
+      @second_call = @foo.bar("hello")
+    end
+    
+    it "should return cached result second time" do
+      @first_call.should == @second_call
+    end
   end
 end
