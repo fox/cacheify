3570832033e63dce5c918a924343e9027e019c53
Sasa Brankovic <sasa@hakeraj.com>
2010-02-20 19:32:22 +0100

    Some docs

--- lib/cacheify.rb
+++ lib/cacheify.rb
@@ -1,7 +1,17 @@
 require 'active_support'
 require 'digest/md5'
 
+##
+# Enables caching of method calls
 module Cacheify
+  ##
+  # Set cache store to be used by Cachify (or leave it be to use defaults).
+  # 
+  # The default is FileStore tmp/cacheify or, if you're running 
+  # inside Rails app, whatever Rails is using.
+  #
+  # === Parameters
+  # store options as understood by ActiveSupport::Cache#lookup_store
   def self.cache_store=(store_option)
     @@cache_store = ActiveSupport::Cache.lookup_store(store_option)
   end
@@ -10,6 +20,15 @@ module Cacheify
     @cache_store ||= defined?(Rails) ? Rails.cache : ActiveSupport::Cache.lookup_store(:file_store, "tmp/cacheify")
   end
 
+  ##
+  # Cache results of methods.
+  #
+  # === Parameters
+  # * method names to cache
+  # * optionally, last argument can be a hash of options passed to ActiveSupport::Cache::Store for caching. 
+  #
+  # === Example
+  # cache_method :big_calculation, :expires_in => 5.minutes
   def cache_method(*symbols)
     options = symbols.last.is_a?(Hash) ? symbols.delete(symbols.last) : {}
 
