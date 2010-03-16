94a79491b775c665d5550e08767e9e0a07f31fd6
Sasa Brankovic <sasa@hakeraj.com>
2010-03-16 11:19:39 +0100

    Works as a singleton now

--- lib/cacheify.rb
+++ lib/cacheify.rb
@@ -30,20 +30,26 @@ module Cacheify
   # === Example
   # cache_method :big_calculation, :expires_in => 5.minutes
   def cacheify(*symbols)
-    options = symbols.extract_options!
+    (self.is_a?(Class) ? self : metaclass).class_eval do
+      options = symbols.extract_options!
 
-    symbols.each do |method|
-      non_cached_method = "_non_cached_#{method}".to_sym 
-      return if respond_to? non_cached_method # cached already, skip it
-      
-      alias_method non_cached_method, method
-      
-      define_method(method) do |*args, &block|
-        cache_name = Digest::MD5.hexdigest("#{self.class.name}#{method}#{args.inspect}")
-        marshalled_result = Cacheify.cache.fetch(cache_name, options) do 
-          send(non_cached_method, *args, &block)
+      symbols.each do |method|
+        non_cached_method = "_non_cached_#{method}".to_sym 
+        return if respond_to? non_cached_method # cached already, skip it
+        
+        alias_method non_cached_method, method
+        
+        define_method(method) do |*args, &block|
+          cache_name = Digest::MD5.hexdigest "#{self.class.name}#{method}#{args.inspect}"
+          marshalled_result = Cacheify.cache.fetch(cache_name, options) do 
+            send(non_cached_method, *args, &block)
+          end
         end
       end
     end
+  end
+
+  def self.included(obj)
+    obj.extend Cacheify
   end
 end
--- spec/cacheify_spec.rb
+++ spec/cacheify_spec.rb
@@ -6,6 +6,7 @@ class Foo
   def bar(something = nil)
     "#{rand(99999999)}#{something}"
   end
+  
   def rab(something = nil)
     "#{rand(99999999)}#{something}"
   end
@@ -14,6 +15,12 @@ end
 class Hoo
   def bar(something = nil)
     "#{rand(99999999)}#{something}"
+  end
+end
+
+class Baz
+  def boo
+    "#{rand(99999999)}"
   end
 end
 
@@ -33,6 +40,7 @@ describe "Cacheify" do
     @foo_bar_diff_args_1_call = foo.bar("howdy")
     @foo_rab_1_call = foo.rab("hello")
     @hoo_bar_1_call = hoo.bar("hello")
+    @hoo_bar_2_call = hoo.bar("hello")
   end
 
   it "should return cached result second time" do
@@ -43,5 +51,20 @@ describe "Cacheify" do
     @hoo_bar_1_call.should_not == @foo_bar_1_call
     @foo_rab_1_call.should_not == @foo_bar_1_call
     @foo_bar_diff_args_1_call.should_not == @foo_bar_1_call
+    # @hoo_bar_1_call.should == @hoo_bar_2_call
+  end
+
+  context "Caching of instantiated object" do
+    before do
+      baz = Baz.new
+      baz.extend Cacheify
+      baz.cacheify :boo
+      @baz_boo_1_call = baz.boo
+      @baz_boo_2_call = baz.boo
+    end
+
+    it do
+      @baz_boo_1_call.should == @baz_boo_2_call
+    end
   end
 end
